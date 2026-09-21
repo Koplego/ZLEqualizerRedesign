d74fdaf4314d30ed365dfc03a8f67c93e1cd44a3
Koplego <75549776+Koplego@users.noreply.github.com>
2026-09-21 19:58:34 +0200

    v1.3: refine output controls into quiet glass utility sheet

--- source/panel/curve_panel/output_panel/output_panel.cpp
+++ source/panel/curve_panel/output_panel/output_panel.cpp
@@ -2,10 +2,6 @@
 // This file is part of ZLEqualizer
 //
 // ZLEqualizer is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License Version 3 as published by the Free Software Foundation.
-//
-// ZLEqualizer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License along with ZLEqualizer. If not, see <https://www.gnu.org/licenses/>.
 
 #include "output_panel.hpp"
 #include "BinaryData.h"
@@ -13,67 +9,42 @@
 namespace zlpanel {
     OutputPanel::OutputPanel(PluginProcessor& p, zlgui::UIBase& base,
                              const multilingual::TooltipHelper& tooltip_helper) :
-        p_ref_(p), base_(base), updater_(),
-        control_background_(base),
-        name_laf_(base),
-        gain_label_("", "GAIN"),
-        scale_label_("", "SCALE"),
-        gain_slider_("", base_,
-                     tooltip_helper.getToolTipText(multilingual::kOutputGain), 1.25f),
-        gain_attach_(gain_slider_.getSlider1(), p.parameters_,
-                     zlp::POutputGain::kID, updater_),
-        scale_slider_("", base,
-                      tooltip_helper.getToolTipText(multilingual::kGainScale), 1.25f),
-        scale_attach_(scale_slider_.getSlider1(), p.parameters_,
-                      zlp::PGainScale::kID, updater_),
-        sgc_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_s_svg,
-                                                          BinaryData::dline_s_svgSize)),
-        sgc_button_(base, sgc_drawable_.get(), sgc_drawable_.get(),
-                    tooltip_helper.getToolTipText(multilingual::kStaticGC)),
-        sgc_attach_(sgc_button_.getButton(), p.parameters_,
-                    zlp::PStaticGain::kID, updater_),
-        lm_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_l_svg,
-                                                         BinaryData::dline_l_svgSize)),
-        lm_button_(base, lm_drawable_.get(), lm_drawable_.get(),
-                   tooltip_helper.getToolTipText(multilingual::kLoudnessGC)),
-        agc_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_a_svg,
-                                                          BinaryData::dline_a_svgSize)),
-        agc_button_(base, agc_drawable_.get(), agc_drawable_.get(),
-                    tooltip_helper.getToolTipText(multilingual::kAutoGC)),
-        agc_attach_(agc_button_.getButton(), p.parameters_,
-                    zlp::PAutoGain::kID, updater_),
-        phase_drawable_(juce::Drawable::createFromImageData(BinaryData::phase_svg,
-                                                            BinaryData::phase_svgSize)),
-        phase_button_(base, phase_drawable_.get(), phase_drawable_.get(),
-                      tooltip_helper.getToolTipText(multilingual::kPhaseFlip)),
-        phase_attach_(phase_button_.getButton(), p.parameters_,
-                      zlp::PPhaseFlip::kID, updater_),
+        p_ref_(p), base_(base), updater_(), control_background_(base), name_laf_(base),
+        gain_label_("", "Output"), scale_label_("", "Scale"),
+        gain_slider_("", base_, tooltip_helper.getToolTipText(multilingual::kOutputGain), 1.25f),
+        gain_attach_(gain_slider_.getSlider1(), p.parameters_, zlp::POutputGain::kID, updater_),
+        scale_slider_("", base, tooltip_helper.getToolTipText(multilingual::kGainScale), 1.25f),
+        scale_attach_(scale_slider_.getSlider1(), p.parameters_, zlp::PGainScale::kID, updater_),
+        sgc_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_s_svg, BinaryData::dline_s_svgSize)),
+        sgc_button_(base, sgc_drawable_.get(), sgc_drawable_.get(), tooltip_helper.getToolTipText(multilingual::kStaticGC)),
+        sgc_attach_(sgc_button_.getButton(), p.parameters_, zlp::PStaticGain::kID, updater_),
+        lm_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_l_svg, BinaryData::dline_l_svgSize)),
+        lm_button_(base, lm_drawable_.get(), lm_drawable_.get(), tooltip_helper.getToolTipText(multilingual::kLoudnessGC)),
+        agc_drawable_(juce::Drawable::createFromImageData(BinaryData::dline_a_svg, BinaryData::dline_a_svgSize)),
+        agc_button_(base, agc_drawable_.get(), agc_drawable_.get(), tooltip_helper.getToolTipText(multilingual::kAutoGC)),
+        agc_attach_(agc_button_.getButton(), p.parameters_, zlp::PAutoGain::kID, updater_),
+        phase_drawable_(juce::Drawable::createFromImageData(BinaryData::phase_svg, BinaryData::phase_svgSize)),
+        phase_button_(base, phase_drawable_.get(), phase_drawable_.get(), tooltip_helper.getToolTipText(multilingual::kPhaseFlip)),
+        phase_attach_(phase_button_.getButton(), p.parameters_, zlp::PPhaseFlip::kID, updater_),
         lookahead_label_("", "Lookahead"),
-        lookahead_slider_("", base,
-                          tooltip_helper.getToolTipText(multilingual::kLookahead)),
-        lookahead_attach_(lookahead_slider_.getSlider(), p.parameters_,
-                          zlp::PLookahead::kID, updater_) {
-        juce::ignoreUnused(p_ref_, base_, tooltip_helper);
+        lookahead_slider_("", base, tooltip_helper.getToolTipText(multilingual::kLookahead)),
+        lookahead_attach_(lookahead_slider_.getSlider(), p.parameters_, zlp::PLookahead::kID, updater_) {
 
         control_background_.setBufferedToImage(true);
         addAndMakeVisible(control_background_);
 
-        name_laf_.setFontScale(1.5f);
-
-        gain_label_.setLookAndFeel(&name_laf_);
-        gain_label_.setJustificationType(juce::Justification::centred);
-        gain_label_.setBufferedToImage(true);
-        addAndMakeVisible(gain_label_);
-
-        scale_label_.setLookAndFeel(&name_laf_);
-        scale_label_.setJustificationType(juce::Justification::centred);
-        scale_label_.setBufferedToImage(true);
-        addAndMakeVisible(scale_label_);
+        name_laf_.setFontScale(.78f);
+        for (auto* l : {&gain_label_, &scale_label_}) {
+            l->setLookAndFeel(&name_laf_);
+            l->setJustificationType(juce::Justification::centred);
+            l->setAlpha(.74f);
+            l->setBufferedToImage(true);
+            addAndMakeVisible(*l);
+        }
 
         gain_slider_.setComponentID(zlp::POutputGain::kID);
         gain_slider_.setBufferedToImage(true);
         addAndMakeVisible(gain_slider_);
-
         scale_slider_.setComponentID(zlp::PGainScale::kID);
         scale_slider_.setBufferedToImage(true);
         addAndMakeVisible(scale_slider_);
@@ -92,13 +63,14 @@ namespace zlpanel {
         };
 
         for (auto& b : {&sgc_button_, &lm_button_, &agc_button_, &phase_button_}) {
-            b->setImageAlpha(.5f, .75f, 1.f, 1.f);
+            b->setImageAlpha(.34f, .56f, .92f, .92f);
             b->setBufferedToImage(true);
             addAndMakeVisible(b);
         }
 
         lookahead_label_.setLookAndFeel(&name_laf_);
-        lookahead_label_.setJustificationType(juce::Justification::centredRight);
+        lookahead_label_.setJustificationType(juce::Justification::centredLeft);
+        lookahead_label_.setAlpha(.68f);
         lookahead_label_.setBufferedToImage(true);
         addAndMakeVisible(lookahead_label_);
 
@@ -116,62 +88,61 @@ namespace zlpanel {
     }
 
     int OutputPanel::getIdealWidth() const {
-        const auto font_size = base_.getFontSize();
-        const auto slider_width = getSliderWidth(font_size);
-        const auto padding = getPaddingSize(font_size);
-        return 5 * padding + 2 * slider_width;
+        const auto font = base_.getFontSize();
+        return juce::jmax(juce::roundToInt(font * 18.0f), 2 * getSliderWidth(font) + 4 * getPaddingSize(font));
     }
 
     int OutputPanel::getIdealHeight() const {
-        const auto font_size = base_.getFontSize();
-        const auto slider_width = getSliderWidth(font_size);
-        const auto button_height = getButtonSize(font_size);
-        const auto padding = getPaddingSize(font_size);
-        return 5 * padding + 3 * button_height + slider_width;
+        const auto font = base_.getFontSize();
+        const auto slider = getSliderWidth(font);
+        const auto row = getButtonSize(font);
+        const auto padding = getPaddingSize(font);
+        return slider + 3 * row + 4 * padding;
     }
 
     void OutputPanel::resized() {
-        const auto font_size = base_.getFontSize();
-        const auto slider_width = getSliderWidth(font_size);
-        const auto button_height = getButtonSize(font_size);
-        const auto padding = getPaddingSize(font_size);
+        const auto font = base_.getFontSize();
+        const auto slider_width = getSliderWidth(font);
+        const auto row = getButtonSize(font);
+        const auto padding = getPaddingSize(font);
 
         auto bound = getLocalBounds();
         control_background_.setBounds(bound);
-
         bound.reduce(2 * padding, padding);
+
+        auto labels = bound.removeFromTop(row * 3 / 4);
+        const auto control_gap = juce::jmax(padding, bound.getWidth() - 2 * slider_width);
+        gain_label_.setBounds(labels.removeFromLeft(slider_width));
+        labels.removeFromLeft(juce::jmin(control_gap, labels.getWidth()));
+        scale_label_.setBounds(labels.removeFromRight(juce::jmin(slider_width, labels.getWidth())));
+        bound.removeFromTop(padding / 3);
+
+        auto sliders = bound.removeFromTop(slider_width);
+        gain_slider_.setBounds(sliders.removeFromLeft(slider_width));
+        scale_slider_.setBounds(sliders.removeFromRight(slider_width));
+        bound.removeFromTop(padding / 2);
+
         {
-            auto t_bound = bound.removeFromTop(button_height);
-            gain_label_.setBounds(t_bound.removeFromLeft(slider_width));
-            scale_label_.setBounds(t_bound.removeFromRight(slider_width));
+            auto actions = bound.removeFromTop(row);
+            const auto icon = juce::jmin(row, juce::roundToInt(font * 1.95f));
+            const auto gap = juce::jmax(2, (actions.getWidth() - 4 * icon) / 5);
+            actions.removeFromLeft(gap);
+            sgc_button_.setBounds(actions.removeFromLeft(icon)); actions.removeFromLeft(gap);
+            lm_button_.setBounds(actions.removeFromLeft(icon)); actions.removeFromLeft(gap);
+            agc_button_.setBounds(actions.removeFromLeft(icon)); actions.removeFromLeft(gap);
+            phase_button_.setBounds(actions.removeFromLeft(icon));
         }
-        bound.removeFromTop(padding);
+        bound.removeFromTop(padding / 2);
+
         {
-            auto t_bound = bound.removeFromTop(slider_width);
-            gain_slider_.setBounds(t_bound.removeFromLeft(slider_width));
-            scale_slider_.setBounds(t_bound.removeFromRight(slider_width));
-        }
-        bound.removeFromTop(padding);
-        {
-            auto t_bound = bound.removeFromTop(button_height);
-            const auto h_padding = (t_bound.getWidth() - 4 * button_height) / 3;
-            sgc_button_.setBounds(t_bound.removeFromLeft(button_height));
-            t_bound.removeFromLeft(h_padding);
-            lm_button_.setBounds(t_bound.removeFromLeft(button_height));
-            t_bound.removeFromLeft(h_padding);
-            phase_button_.setBounds(t_bound.removeFromRight(button_height));
-            t_bound.removeFromRight(h_padding);
-            agc_button_.setBounds(t_bound.removeFromRight(button_height));
-        }
-        bound.removeFromTop(padding);
-        {
-            auto t_bound = bound.removeFromTop(button_height);
-            lookahead_slider_.setBounds(t_bound.removeFromRight(t_bound.getWidth() / 3));
-            t_bound.removeFromRight(padding);
-            lookahead_label_.setBounds(t_bound);
+            auto lookahead = bound.removeFromTop(row);
+            const auto label_w = juce::jmax(juce::roundToInt(font * 5.0f), lookahead.getWidth() / 2);
+            lookahead_label_.setBounds(lookahead.removeFromLeft(label_w));
+            lookahead.removeFromLeft(padding / 2);
+            lookahead_slider_.setBounds(lookahead);
         }
 
-        const auto dragging_distance = getSliderDraggingDistance(font_size);
+        const auto dragging_distance = getSliderDraggingDistance(font);
         gain_slider_.setMouseDragSensitivity(dragging_distance);
         scale_slider_.setMouseDragSensitivity(dragging_distance);
         lookahead_slider_.setMouseDragSensitivity(dragging_distance);
