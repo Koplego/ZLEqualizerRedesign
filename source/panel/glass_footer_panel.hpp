997c46208843d8884af4374cb5687fa6d810dd3c
Koplego <75549776+Koplego@users.noreply.github.com>
2026-09-21 20:53:23 +0200

    v1.3: separate analyzer power from analyzer menu

--- source/panel/glass_footer_panel.hpp
+++ source/panel/glass_footer_panel.hpp
@@ -17,19 +17,24 @@ namespace zlpanel {
         void repaintCallbackSlow();
 
     private:
+        PluginProcessor& p_ref_;
         zlgui::UIBase& base_;
         zlgui::attachment::ComponentUpdater updater_{};
 
-        zlgui::button::ClickTextButton analyzer_button_;
+        // Analyzer is deliberately split into two interactions:
+        // clicking the word opens its detail sheet, while the switch controls whether
+        // any spectrum trace is rendered at all.
+        zlgui::button::ClickTextButton analyzer_menu_button_;
+        zlgui::button::ClickTextButton analyzer_toggle_button_;
         zlgui::button::ClickTextButton pre_button_;
         zlgui::button::ClickTextButton post_button_;
+        zlgui::button::ClickTextButton side_button_;
         zlgui::attachment::ButtonAttachment<true> pre_attach_;
         zlgui::attachment::ButtonAttachment<true> post_attach_;
+        zlgui::attachment::ButtonAttachment<true> side_attach_;
 
         zlgui::combobox::CompactCombobox speed_box_;
         zlgui::attachment::ComboBoxAttachment<true> speed_attach_;
-
-        zlgui::button::ClickTextButton output_button_;
 
         zlgui::combobox::CompactCombobox phase_box_;
         zlgui::attachment::ComboBoxAttachment<true> phase_attach_;
@@ -37,9 +42,14 @@ namespace zlpanel {
         juce::Rectangle<int> processing_label_bound_{};
         juce::Rectangle<int> spectrum_label_bound_{};
         juce::Rectangle<int> analyzer_group_bound_{};
-        juce::Rectangle<int> tools_group_bound_{};
         juce::Rectangle<int> processing_group_bound_{};
 
+        unsigned int analyzer_restore_mask_{0b011u};
+
         void stylePill(zlgui::button::ClickTextButton& button);
+        bool isAnalyzerEnabled() const;
+        unsigned int getAnalyzerMask() const;
+        void setAnalyzerMask(unsigned int mask);
+        void toggleAnalyzerEnabled();
     };
 }
